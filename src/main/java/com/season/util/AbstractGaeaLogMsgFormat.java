4e5e46dcd004dbda30bb1c2bada3503a1eb61534
PH-FSY <69564735+PH-FSY@users.noreply.github.com>
2020-09-14 16:58:54 +0800

    Create AbstractGaeaLogMsgFormat.java

---
+++ src/main/java/com/season/util/AbstractGaeaLogMsgFormat.java
@@ -0,0 +1,10 @@
+package com.season.util;
+
+public abstract class AbstractGaeaLogMsgFormat {
+
+    protected static final String INFO_DIV = "-/-";
+
+    protected static final String KV_DIV = "-:-";
+
+    protected static final String LOG_ARGUMENT = "{}";
+}
